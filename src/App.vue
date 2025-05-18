022f58ee8715e3c2ca10c6fc86cd26b27f03d159
liyang <liyang@qq.com>
2025-05-18 23:09:10 +0800

    017_项目中styleLint工具配置

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,16 @@
 </template>
 
 <script setup lang="ts">
-
+const str = "我爱你祖国";
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.box{
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1{
+    color: white;
+  }
+}
 </style>
